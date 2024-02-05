4d2bb167d0ff8871ac4b00c8e702775e14c0de88
CodeSandbox <support@codesandbox.io>
2024-02-05 15:27:54 +0000

    Sandbox snapshot commit

--- e-rezept.php
+++ e-rezept.php
@@ -44,9 +44,13 @@
     Ich habe die Datenschutzerlärung gelesen
   </label>
 </div>
-<div class="input-group">
+<div  class="input-group">
   <input type="text" aria-label="Vorname" class="form-control" placeholder="Vorname" id="first-name">
   <input type="text" aria-label="Nachname" class="form-control" placeholder="Nachname" id="last-name">
+</div>
+<div  class="input-group">
+  <input type="text" aria-label="Straße und Hausnummer" class="form-control" placeholder="Straße und Hausnummer" id="street">
+  <input type="text" aria-label="Lieferort" class="form-control" placeholder="Ort" id="place">
 </div>
     <div>
       <button id="submit-button" class="btn" style="background-color:#dd3545">Absenden</button>
