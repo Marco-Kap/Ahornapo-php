be7e3abafaad8865dbe9c33d54166b2916ddfbbd
CodeSandbox <support@codesandbox.io>
2024-02-05 15:10:04 +0000

    Sandbox snapshot commit

--- e-rezept.php
+++ e-rezept.php
@@ -25,7 +25,7 @@
             <p>Außerdem haben Sie die Möglichkeit Ihr Rezept zu fotografieren oder zu scannen und direkt bei uns hochladen</p>
 
             <div class="card">
-        <form class="px-4 m-2" style="max-width: 60vw" method="get" name="form" action="submit.php">
+        <form class="px-4 m-2" style="max-width: 120vw" method="get" name="form" action="submit.php">
             <div class="row">
             <label class="col align-self-center py-2 rounded" style="background-color:#dd3545" for="image_upload">Rezepte hochladen</label>
             <input type="file" id="image_upload" name="image_uploads" accept=".jpg, .jpeg, .png" multiple>
